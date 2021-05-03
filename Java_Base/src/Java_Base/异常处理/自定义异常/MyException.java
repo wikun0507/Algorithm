691a399405eb56610c9179a76cd3b4aeb3b1c6a0
“WiKunZ <122010396@qq.com>
2021-05-03 19:45:29 +0800

    New

--- Java_Base/src/Java_Base/异常处理/自定义异常/MyException.java
+++ Java_Base/src/Java_Base/异常处理/自定义异常/MyException.java
@@ -8,7 +8,7 @@ package Java_Base.异常处理.自定义异常;
  * 2.提供全局常量：serialVersionUID
  * 3.提供重载的构造器
  */
-public class MyException extends RuntimeException{
+public class MyException extends Exception{
     static final long serialVersionUID = -7034897190745766939L;
     public MyException(){
 
