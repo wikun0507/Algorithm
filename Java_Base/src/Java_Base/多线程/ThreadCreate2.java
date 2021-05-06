6983d3fc7265e481818a5e5eb6e8de6152557981
“WiKunZ <122010396@qq.com>
2021-05-06 18:58:12 +0800

    New

---
+++ Java_Base/src/Java_Base/多线程/ThreadCreate2.java
@@ -0,0 +1,42 @@
+package Java_Base.多线程;
+
+/**
+ * @Author WiKunZ
+ * @Date 2021/5/6-18:31
+ *
+ * 创建多线程的第二种方式
+ * 1.创建一个实现了Runnable接口的类
+ * 2.实现类去实现Runnable中的抽象方法：run()
+ * 3.创建实现类的对象
+ * 4.将此对象作为参数传递到Thread类的构造器中，创建Thread类的对象
+ * 5.通过Thread类的对象调用start()
+ */
+public class ThreadCreate2 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new MyThread2());
+        thread.start();
+        Thread thread1 = new Thread(new MyThread4());
+        thread1.start();
+    }
+}
+
+class MyThread2 implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i = 1;i<=100;i++){
+            if(i%2==0)
+            System.out.println(Thread.currentThread().getName()+": "+i);
+        }
+    }
+}
+
+class MyThread4 implements Runnable{
+    @Override
+    public void run() {
+        for(int i = 1;i<=100;i++){
+            if(i%2!=0)
+                System.out.println(Thread.currentThread().getName()+": "+i);
+        }
+    }
+}
--- Java_Base/src/Java_Base/多线程/练习/WindowTest.java
+++ Java_Base/src/Java_Base/多线程/练习/WindowTest.java
@@ -11,18 +11,41 @@ public class WindowTest {
 
 
     public static void main(String[] args) {
-        Window t1 = new Window();
-        Window t2 = new Window();
-        Window t3 = new Window();
-        t1.setName("窗口1");
-        t2.setName("窗口2");
-        t3.setName("窗口3");
-        t1.start();
-        t2.start();
-        t3.start();
+        /** ticket加static的方式 */
+//        Window t1 = new Window();
+//        Window t2 = new Window();
+//        Window t3 = new Window();
+//        t1.setName("窗口1");
+//        t2.setName("窗口2");
+//        t3.setName("窗口3");
+//        t1.start();
+//        t2.start();
+//        t3.start();
+
+        /** ticket不加static的方式 */
+//        Window2 window2 = new Window2();
+//        Thread thread4 = new Thread(window2);
+//        Thread thread5 = new Thread(window2);
+//        Thread thread6 = new Thread(window2);
+//        thread4.start();
+//        thread5.start();
+//        thread6.start();
+//
     }
 }
-
+class Window2 implements Runnable{
+    public int ticket = 200;
+    @Override
+    public void run() {
+        while (true){
+            if(ticket>0){
+                System.out.println(Thread.currentThread().getName()+":买票，票号为"+ticket--);
+            }else{
+                break;
+            }
+        }
+    }
+}
 class Window extends Thread{
     public static int ticket = 200;
     @Override
