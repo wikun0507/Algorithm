abad027cd848ea1c2fbc7bdee08c0372fd619ed2
“WiKunZ <122010396@qq.com>
2021-05-11 20:20:14 +0800

    New

---
+++ src/LeetCode/数组/解码异或后的排列/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.数组.解码异或后的排列;
+
+/**
+ * @Author WiKunZ
+ * @Date 2021/5/11-11:03
+ */
+public class Solution {
+    public int[] decode(int[] encoded) {
+        return new int[5];
+    }
+    public static void main(String[] args) {
+
+    }
+}
