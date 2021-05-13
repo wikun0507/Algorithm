6655587f404e776c89173a180d1fcff867b84381
“WiKunZ <122010396@qq.com>
2021-05-13 17:46:41 +0800

    New

--- Java_Base/src/Java_Base/多态性/Woman.java
+++
@@ -1,8 +0,0 @@
-package Java_Base.多态性;
-
-public class Woman extends Person{
-    @Override
-    public void eat(){
-        System.out.println("我喜欢吃J8");
-    }
-}
--- Java_Base/src/Java_Base/练习/Practice_3.java
+++
@@ -1,7 +0,0 @@
-package Java_Base.练习;
-
-public class Practice_3 {
-    public static void main(String[] args) {
-//        int[] a = new int[4];
-    }
-}
